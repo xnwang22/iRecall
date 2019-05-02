e2741396437e9883eb0e2b1805a6f24a6ccb9f42
AVS <alxgknet@live.com>
2019-05-02 12:06:06 -0400

    Update test.py

--- app/test.py
+++ app/test.py
@@ -1,6 +1,13 @@
 import iRecall
+import os
 
 if __name__ == "__main__":
+    root_folder = './data/'
+    try:
+        os.stat(root_folder)
+    except:
+        os.mkdir(root_folder)
+
     dictionary = iRecall.load_dictionary('./data/')
     maxConfidence = 10000
     iRecall.process_image('test.jpg', dictionary, './data/', maxConfidence)
