19d00df2ff95b0a560ad8417ec5e50698d5b0056
AVS <alxgknet@live.com>
2019-05-02 12:06:33 -0400

    Update train.py

--- app/train.py
+++ app/train.py
@@ -1,5 +1,11 @@
 import iRecall
+import os
 
 if __name__ == "__main__":
-    iRecall.train_model('./data/')
+    root_folder = './data/'
+    try:
+        os.stat(root_folder)
+    except:
+        os.mkdir(root_folder)
+    iRecall.train_model(root_folder)
 
